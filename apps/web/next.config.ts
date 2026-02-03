53e03aa652c81282b1f22e887ad429e4044f510d
Eric Riedel <8659979+emriedel@users.noreply.github.com>
2026-02-02 19:38:23 -0800

    Fix images on home screen

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nerdcube.games',
+        pathname: '/icons/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
